99c031584481a820a773221a63dce805d973062e
SolomonM23 <solomonmalone@yahoo.com>
2024-02-17 20:43:34 -0500

    basic implementation of the sidebar menu goes away when menu button is clicked

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 let menuBtn = document.querySelector('#menu-btn');
-menuBtn.addEventListener('click', )
+menuBtn.addEventListener('click', function() {
+    let sidebarMenu = document.querySelector('.sidebar-menu');
+
+    sidebarMenu.classList.toggle('hidden');
+})
