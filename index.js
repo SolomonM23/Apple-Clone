d9d6d72b973da01831ed64f7e813b6e551dd0e79
SolomonM23 <solomonmalone@yahoo.com>
2024-02-20 18:11:48 -0500

    sidebar exit button and nav menu button toggle visibility

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-// nav menu button hides/shows sidebar when clicked
+// mobile nav menu button 
 let menuBtn = document.querySelector('#menu-btn');
 let sidebarMenu = document.querySelector('.sidebar-menu');
 
-menuBtn.addEventListener('click', function() {
-
+function sidebarVisible() {
     sidebarMenu.classList.toggle('hidden');
     sidebarMenu.classList.toggle('visible');
-})
+    navBar.classList.toggle('nav-hidden')
+    navBar.classList.toggle('nav-visible')
+}
+menuBtn.addEventListener('click', sidebarVisible);
+
+//mobile sidebar exit button 
+let sidebarClose = document.querySelector('.exit');
+let navBar = document.querySelector('.navbar');
+
+sidebarClose.addEventListener('click', sidebarVisible);
