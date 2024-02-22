1c109e4d51701f001d88df234443a41e65290c2e
SolomonM23 <solomonmalone@yahoo.com>
2024-02-22 18:42:50 -0500

    sub menu list reveals smooth and one by one

--- index.js
+++ index.js
@@ -20,13 +20,25 @@
 let menuBtn = document.querySelector('#menu-btn');
 let nav = document.querySelector('.navbar');
 let subMenu = document.querySelector('.sub-menu-mobile');
+let subMenuList = document.querySelectorAll('.sub-menu-mobile a')
 
 menuBtn.addEventListener('click', function() {
     nav.style.height = (nav.style.height === '48px')? '100vh' : '48px';
 
     if (nav.style.height === '100vh') {
         subMenu.classList.remove('hidden');
+
+        // Use setTimeout to reveal each anchor item one after the other
+        subMenuList.forEach((item, index) => {
+        setTimeout(() => {
+          item.style.opacity = 1;
+        }, index * 75); // Adjust the delay (in milliseconds) between items
+      });
     } else {
         subMenu.classList.add('hidden');
+
+        subMenuList.forEach(item => {
+            item.style.opacity = 0; // Hide the items when the menu is collapsed
+          });
     }
 })
