b0311e23bb7ffdd2fabe2b741f8957f22d5c42ce
SolomonM23 <solomonmalone@yahoo.com>
2024-02-22 17:59:13 -0500

    the mobile sub menu is now in the navbar and shows when the menu button is clicked and the navbar height covers the full viewport

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
-// mobile nav menu button 
+// // mobile nav menu button 
+// let menuBtn = document.querySelector('#menu-btn');
+// let sidebarMenu = document.querySelector('.sub-menu-mobile');
+
+// function sidebarVisible() {
+//     sidebarMenu.classList.toggle('hidden');
+//     sidebarMenu.classList.toggle('visible');
+//     navBar.classList.toggle('nav-hidden')
+//     navBar.classList.toggle('nav-visible')
+// }
+// menuBtn.addEventListener('click', sidebarVisible);
+
+// //mobile sidebar exit button 
+// let sidebarClose = document.querySelector('.exit');
+// let navBar = document.querySelector('.navbar');
+
+// sidebarClose.addEventListener('click', sidebarVisible);
+
+
 let menuBtn = document.querySelector('#menu-btn');
-let sidebarMenu = document.querySelector('.sub-menu-mobile');
+let nav = document.querySelector('.navbar');
+let subMenu = document.querySelector('.sub-menu-mobile');
 
-function sidebarVisible() {
-    sidebarMenu.classList.toggle('hidden');
-    sidebarMenu.classList.toggle('visible');
-    navBar.classList.toggle('nav-hidden')
-    navBar.classList.toggle('nav-visible')
-}
-menuBtn.addEventListener('click', sidebarVisible);
+menuBtn.addEventListener('click', function() {
+    nav.style.height = (nav.style.height === '48px')? '100vh' : '48px';
 
-//mobile sidebar exit button 
-let sidebarClose = document.querySelector('.exit');
-let navBar = document.querySelector('.navbar');
-
-sidebarClose.addEventListener('click', sidebarVisible);
-
-
-// let menuBtn = document.querySelector('.menuBtn');
-// let nav = document.querySelector('.fake-nav');
-// let content = document.querySelector('.fake-content-overlay');
-
-// menuBtn.addEventListener('click', function() {
-//     nav.style.height = (nav.style.height === '5rem')? '50rem' : '5rem';
-
-//     if (nav.style.height === '50rem') {
-//         content.classList.add('fake-visible');
-//         content.classList.remove('fake-hidden');
-//     } else {
-//         content.classList.add('fake-hidden');
-//         content.classList.remove('fake-visible');
-//     }
-// })
+    if (nav.style.height === '100vh') {
+        subMenu.classList.add('nav-visible');
+        subMenu.classList.remove('nav-hidden');
+    } else {
+        subMenu.classList.add('nav-hidden');
+        subMenu.classList.remove('nav-visible');
+    }
+})
