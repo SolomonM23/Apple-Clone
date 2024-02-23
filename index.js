604f2c970cb0a5efeb7ddc47315bd2605fde43e3
SolomonM23 <solomonmalone@yahoo.com>
2024-02-22 19:03:44 -0500

    removed extra comments from js, and the exit button from the sub menu html

--- index.js
+++ index.js
@@ -1,22 +1,4 @@
-// // mobile nav menu button 
-// let menuBtn = document.querySelector('#menu-btn');
-// let sidebarMenu = document.querySelector('.sub-menu-mobile');
-
-// function sidebarVisible() {
-//     sidebarMenu.classList.toggle('hidden');
-//     sidebarMenu.classList.toggle('visible');
-//     navBar.classList.toggle('nav-hidden')
-//     navBar.classList.toggle('nav-visible')
-// }
-// menuBtn.addEventListener('click', sidebarVisible);
-
-// //mobile sidebar exit button 
-// let sidebarClose = document.querySelector('.exit');
-// let navBar = document.querySelector('.navbar');
-
-// sidebarClose.addEventListener('click', sidebarVisible);
-
-
+// mobile nav menu button 
 let menuBtn = document.querySelector('#menu-btn');
 let nav = document.querySelector('.navbar');
 let subMenu = document.querySelector('.sub-menu-mobile');
