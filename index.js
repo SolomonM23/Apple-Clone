371160756e33dbcd394f1af0299bbdfcc8267ee1
SolomonM23 <solomonmalone@yahoo.com>
2024-02-24 15:22:32 -0500

    separated each event listener for menuBtn. the x icon not working when nav is expanded

--- index.js
+++ index.js
@@ -6,49 +6,14 @@ let subMenuList = document.querySelectorAll('.sub-menu-mobile a')
 // let logo = document.querySelector('.fa-apple');
 // let searchBtn = document.querySelector('.fa-magnifying-glass');
 // let cartBtn = document.querySelector('.fa-bag-shopping');
+const isExpanded = nav.style.height === '100vh';
 
+// Toggle the height of the nav
 menuBtn.addEventListener('click', function() {
     // Check the current height of the nav
-    const isExpanded = nav.style.height === '100vh';
 
-    // Toggle the height of the nav
     nav.style.height = isExpanded ? '48px' : '100vh';
 
-    // Toggle the icon based on the nav height
-    if (isExpanded) {
-        // If nav is collapsing, hide sub-menu items
-        subMenu.classList.add('hidden');
-        subMenuList.forEach(item => {
-            item.style.opacity = 0;
-        });
-    } else {
-        // If nav is expanding, reveal sub-menu items with a delay
-        subMenu.classList.remove('hidden');
-        subMenuList.forEach((item, index) => {
-            setTimeout(() => {
-                item.style.opacity = 1;
-            }, index * 75);
-        });
-    }
-
-    // Toggle the icon based on the nav height
-    if (isExpanded) {
-        menuBtn.classList.remove('fa-times');
-        menuBtn.classList.add('fa-bars');
-    } else {
-        menuBtn.classList.remove('fa-bars');
-        menuBtn.classList.add('fa-times');
-    }
-
-    // Set the background color based on isExpanded
-    nav.style.backgroundColor = isExpanded ? '/* Your original background color */' : '/* Your desired background color when expanded */';
-
-    // Toggle the expanded class based on isExpanded
-    if (isExpanded) {
-        nav.classList.remove('expanded');
-    } else {
-        nav.classList.add('expanded');
-    }
 
     // //Hide buttons when menu expanded
     // if (isExpanded) {
@@ -60,4 +25,45 @@ menuBtn.addEventListener('click', function() {
     //     searchBtn.classList.remove('invisible');
     //     cartBtn.classList.remove('invisible');
     // }
+});
+
+// Reveal sub menu when nav height is expanded
+menuBtn.addEventListener('click', function(){
+        
+        if (isExpanded) {
+            // If nav is collapsing, hide sub-menu items
+            subMenu.classList.add('hidden');
+            subMenuList.forEach(item => {
+                item.style.opacity = 0;
+            });
+        } else {
+            // If nav is expanding, reveal sub-menu items with a delay
+            subMenu.classList.remove('hidden');
+            subMenuList.forEach((item, index) => {
+                setTimeout(() => {
+                    item.style.opacity = 1;
+                }, index * 75);
+            });
+        }
+});
+
+// Change the menu button if nav height is expanded
+menuBtn.addEventListener('click', function() {
+        
+        if (isExpanded) {
+            menuBtn.classList.remove('fa-times');
+            menuBtn.classList.add('fa-bars');
+        } else {
+            menuBtn.classList.remove('fa-bars');
+            menuBtn.classList.add('fa-times');
+        }
+});
+
+// Set the background color based on isExpanded
+menuBtn.addEventListener('click', function() {
+        if (isExpanded) {
+            nav.classList.remove('expanded');
+        } else {
+            nav.classList.add('expanded');
+        }
 });
